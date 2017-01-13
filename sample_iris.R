d8ef8b62f80163f5805898f72102b41e1a3304e0
garnerat <atg530@gmail.com>
2017-01-13 18:53:47 -0500

    initial commit

---
+++ sample_iris.R
@@ -0,0 +1,9 @@
+#### Random 80/20 Split of Iris Data ####
+
+data("iris")
+
+sample.index <- sample(1:nrow(iris),nrow(iris)*.8)
+
+iris.data <- iris[sample.index,]
+
+iris.holdout <- iris[-sample.index]
